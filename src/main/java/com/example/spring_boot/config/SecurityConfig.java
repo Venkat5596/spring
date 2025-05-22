2c2d8855eeb79f71994ff757d1c117053ad7aa9e
Venkat5596 <154737186+Venkat5596@users.noreply.github.com>
2025-05-22 14:21:45 +0530

    message

--- src/main/java/com/example/spring_boot/config/SecurityConfig.java
+++ src/main/java/com/example/spring_boot/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.spring_boot.config;
 
 import com.example.spring_boot.jwt.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,9 +21,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@ComponentScan(basePackages = "com.example.spring_boot.jwt")
 public class SecurityConfig {
 
-<<<<<<< HEAD
+
     private final JwtFilter jwtFilter;
 
     @Autowired
@@ -34,24 +36,11 @@ public class SecurityConfig {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(15);
     }
-=======
-   private final JwtFilter jwtFilter;
-
-   @Autowired
-   public SecurityConfig(JwtFilter jwtFilter) {
-       this.jwtFilter = jwtFilter;
-   }
-
-   @Bean
-   public BCryptPasswordEncoder passwordEncoder() {
-       return new BCryptPasswordEncoder(15);
-   }
->>>>>>> 94bdb799a41dcf7039a27b4e2efd8e4ae78e581b
 
 
 
 
-<<<<<<< HEAD
+
         private static final String[] SWAGGER_PATHS = {
                 "/swagger-ui/**",
                 "/v3/api-docs/**",
@@ -111,67 +100,8 @@ public class SecurityConfig {
 
         return http.build();
     }
-=======
-       private static final String[] SWAGGER_PATHS = {
-               "/swagger-ui/**",
-               "/v3/api-docs/**",
-               "/swagger-ui.html",
-               "/swagger-resources/**",
-               "/webjars/**",
-               "/configuration/**",
-               "/web/authors/**"
-       };
-
-       private static final String[] PUBLIC_PATHS = {
-               "/register",
-               "/web/login",
-               "/api/web/login",
-               "/api/web/register",
-               "/WEB-INF/jsp/**"
 
 
-       };
-
-   private static final String[] PROTECTED_PATHS = {
-           "/books/**",
-           "/authors/**",
-           "/web/authors/**",
-           "/web/books/**",
-           "/api/web/index"
-   };
-
-   @Bean
-   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-       http
-               .csrf(AbstractHttpConfigurer::disable)
-               .sessionManagement(session ->
-                       session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-               )
-               .authorizeHttpRequests(auth -> auth
-                       .requestMatchers(SWAGGER_PATHS).permitAll()
-                       .requestMatchers(PUBLIC_PATHS).permitAll()
-                       .requestMatchers("/WEB-INF/jsp/**").permitAll()
-                       .requestMatchers("/favicon.ico").permitAll()
-                       .requestMatchers(PROTECTED_PATHS).hasAnyRole("ADMIN", "USER")
-                       .anyRequest().authenticated()
-               )
-               .formLogin(form -> form
-                       .loginPage("/api/web/login")
-                       //.loginProcessingUrl("/api/web/login")
-                       .defaultSuccessUrl("/api/web/index", true)
-                       .failureUrl("/web/login?error=true")
-                       .permitAll()
-               )
-               .logout(logout -> logout
-                       .logoutUrl("/logout")
-                       .logoutSuccessUrl("/web/login")
-                       .permitAll()
-               )
-               .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-
-       return http.build();
-   }
->>>>>>> 94bdb799a41dcf7039a27b4e2efd8e4ae78e581b
 
 //    private static final String[] PUBLIC_PATHS = {
 //            "/register",
@@ -185,17 +115,9 @@ public class SecurityConfig {
 //    };
 
 
-
-<<<<<<< HEAD
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
         return auth.getAuthenticationManager();
     }
 }
-=======
-   @Bean
-   public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
-       return auth.getAuthenticationManager();
-   }
-}
->>>>>>> 94bdb799a41dcf7039a27b4e2efd8e4ae78e581b
+
